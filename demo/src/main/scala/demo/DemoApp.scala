3cd53fad28e0f03eaf264f119fb2df01ddfb05e3
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-03-04 14:07:30 -0300

    Reformat
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- demo/src/main/scala/demo/DemoApp.scala
+++ demo/src/main/scala/demo/DemoApp.scala
@@ -1,6 +1,6 @@
 package demo
 
-import java.time.{OffsetDateTime, Instant, LocalDateTime, ZoneId}
+import java.time.{ Instant, LocalDateTime, OffsetDateTime, ZoneId }
 import java.time.zone.ZoneRulesProvider
 import java.time.format._
 // import java.util.Locale
@@ -10,10 +10,11 @@ object DemoApp {
     println(LocalDateTime.ofInstant(Instant.now, ZoneId.systemDefault).toString())
     println(ZoneRulesProvider.getAvailableZoneIds)
     // Locale.setDefault(Locale.forLanguageTag("fi-FI"))
-    val instant = Instant.ofEpochMilli(0)
-    val datetime = LocalDateTime.ofInstant(Instant.now, ZoneId.of("Europe/Helsinki"))
+    val instant   = Instant.ofEpochMilli(0)
+    val datetime  = LocalDateTime.ofInstant(Instant.now, ZoneId.of("Europe/Helsinki"))
     val formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
-    val odt = OffsetDateTime.parse("2011-12-03T10:15:30+01:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+    val odt =
+      OffsetDateTime.parse("2011-12-03T10:15:30+01:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME)
     val strRepl = odt.format(formatter)
     println(instant)
     println(datetime)

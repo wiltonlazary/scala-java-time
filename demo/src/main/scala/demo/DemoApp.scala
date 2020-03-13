42d140a46c381c6c8b019e343359ca67cdd27a71
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-03-13 11:46:24 -0300

    Expand demo to inclease coverage
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- demo/src/main/scala/demo/DemoApp.scala
+++ demo/src/main/scala/demo/DemoApp.scala
@@ -1,12 +1,35 @@
 package demo
 
-import java.time.{ Instant, LocalDateTime, OffsetDateTime, ZoneId }
+import java.time._
 import java.time.zone.ZoneRulesProvider
 import java.time.format._
 // import java.util.Locale
 
 object DemoApp {
   def main(args: Array[String]): Unit = {
+    val fixedClock = Clock.fixed(Instant.ofEpochSecond(1234567890L), ZoneOffset.ofHours(0))
+
+    val dateClock = LocalDateTime.now(fixedClock)
+
+    val tomorrow = dateClock.plusDays(1)
+
+    val duration = Duration.between(dateClock, tomorrow)
+
+    Instant.now(fixedClock)
+    Instant.parse("2007-12-03T10:15:30.00Z")
+
+    val period = Period.between(dateClock.toLocalDate, tomorrow.toLocalDate)
+    assert(period.get(temporal.ChronoUnit.DAYS) == 1L)
+
+    val date1 = LocalDate.of(2001, 1, 31)
+    val date2 = LocalDate.of(2001, 2, 28)
+    assert(date1.plusMonths(1) == date2)
+    val date3 = date1.`with`(temporal.TemporalAdjusters.next(DayOfWeek.SUNDAY))
+    val date4 = LocalDate.of(2001, 2, 4)
+    assert(date3 == date4)
+
+    val offsetTime = OffsetTime.of(dateClock.toLocalTime, ZoneOffset.ofHours(1))
+
     println(LocalDateTime.ofInstant(Instant.now, ZoneId.systemDefault).toString())
     println(ZoneRulesProvider.getAvailableZoneIds)
     // Locale.setDefault(Locale.forLanguageTag("fi-FI"))
@@ -16,6 +39,13 @@ object DemoApp {
     val odt =
       OffsetDateTime.parse("2011-12-03T10:15:30+01:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME)
     val strRepl = odt.format(formatter)
+
+    println(fixedClock)
+    println(dateClock)
+    println(tomorrow)
+    println(duration)
+    println(period)
+    println(offsetTime)
     println(instant)
     println(datetime)
     println(strRepl)

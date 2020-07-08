637c283ec73d102d8454df8c2f1d8527686a4a91
exoego <ytatsuno.jp@gmail.com>
2020-07-08 21:50:08 +0900

    Replace exists(_ == v) with contains(v) that do not require predicate object

--- core/js/src/main/scala/java/util/TimeZone.scala
+++ core/js/src/main/scala/java/util/TimeZone.scala
@@ -94,7 +94,7 @@ abstract class TimeZone extends Serializable with Cloneable {
 
     val id = getID
     def currentIdStrings(strs: Array[String]): Boolean =
-      atIndex(strs, 0).exists(_ == id)
+      atIndex(strs, 0).contains(id)
 
     val zoneStrings = DateFormatSymbols.getInstance(locale).getZoneStrings
     val zoneName    = zoneStrings.find(currentIdStrings).flatMap { strs =>
